16fbef94d17e08907f02917f61ab0a9e734b2ce6
Fene-87 <fenekayas87@gmail.com>
2023-06-17 22:49:29 +0300

    Fix linters

--- src/components/hotelCard/HotelCard.jsx
+++ src/components/hotelCard/HotelCard.jsx
@@ -4,29 +4,19 @@ import React from 'react';
 import './HotelCard.css';
 
 const HotelCard = ({
-  id,
   room_name,
   description,
-  wifi,
-  room_service,
-  beds,
   image_url,
-}) => {
-  const seeDetails = () => {
-
-  };
-
-  return (
-    <div className="hotel-card">
-      <img src={image_url} alt="Hotel" />
-      <h3>{room_name}</h3>
-      <div className="border-line" />
-      <p className="hotel-description">{description}</p>
-      <div>
-        <FontAwesomeIcon icon={faBell} />
-      </div>
+}) => (
+  <div className="hotel-card">
+    <img src={image_url} alt="Hotel" />
+    <h3>{room_name}</h3>
+    <div className="border-line" />
+    <p className="hotel-description">{description}</p>
+    <div>
+      <FontAwesomeIcon icon={faBell} />
     </div>
-  );
-};
+  </div>
+);
 
 export default HotelCard;
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+const Details = () => (
+  <div>Details</div>
+);
 
 export default Details;
