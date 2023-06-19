3f220ceb64331d340f0bb42fd171754cc4d4092d
aubinleyoung <aubinsimpeze01@gmail.com>
2023-06-19 13:14:11 +0200

    Add style to the reservations cards

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import '../styles/reservations.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetReservation } from '../redux/reservation/reservationListSlice';
 
@@ -15,16 +16,29 @@ const Reservations = () => {
     }));
   }, [dispatch]);
 
-
   return (
     <section className="container">
-      <h1>Reservations</h1>
+      <h1 className="title">Reservations</h1>
+      <br />
       {reservations.map((reservation) => (
-        <div key={reservation.id}>
+        <div key={reservation.id} className="card">
 
-          <p>{reservation.rooms_id}</p>
-          <p>{reservation.from_date}</p>
-          <p>{reservation.to_date}</p>
+          <h2>
+            Room ID:
+            {reservation.rooms_id}
+          </h2>
+          <div className="date">
+            <h3>
+              From date:
+              {reservation.from_date}
+            </h3>
+            <h3>
+              To date:
+              {reservation.to_date}
+            </h3>
+            <br />
+            <button className="btn" type="submit">Cancel reservation</button>
+          </div>
         </div>
       ))}
     </section>
