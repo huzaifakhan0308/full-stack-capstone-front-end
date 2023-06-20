40040483705f604d0b0ff48f029e0fa3e5c61bf7
ohyajk <ohyajk@gmail.com>
2023-06-20 15:17:43 +0530

    Add Cancel Reservation

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import '../styles/reservations.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 import { GetReservation } from '../redux/reservation/reservationListSlice';
+import { fetchHotels } from '../redux/home/homeSlice';
 
 const Reservations = () => {
   const dispatch = useDispatch();
@@ -9,12 +11,37 @@ const Reservations = () => {
 
   const loginUserData = JSON.parse(localStorage.getItem('user_data'));
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     dispatch(GetReservation({
       user_id: loginUserData.user_id,
 
     }));
-  }, [dispatch]);
+    dispatch(fetchHotels(loginUserData.user_id));
+  }, []);
+
+  const { hotels } = useSelector((store) => store.home);
+  console.log(hotels);
+
+  const cancelReservation = async (id) => {
+    const response = await fetch(`https://hotels-reservations.onrender.com/users/${loginUserData.user_id}/reservations/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ password: loginUserData.password }),
+    });
+
+    if (response.ok) {
+      console.log('Reservation successfully canceled');
+      navigate('/');
+    } else {
+      console.error('Failed to cancel reservation');
+    }
+  };
+
+  if (!reservations.length) <h1 className="title">No reservations</h1>;
 
   return (
     <section className="container">
@@ -24,8 +51,8 @@ const Reservations = () => {
         <div key={reservation.id} className="card">
 
           <h2>
-            Room ID:
-            {reservation.rooms_id}
+            Room Name:
+            {hotels[reservation?.rooms_id]?.room_name}
           </h2>
           <div className="date">
             <h3>
@@ -37,7 +64,7 @@ const Reservations = () => {
               {reservation.to_date}
             </h3>
             <br />
-            <button className="btn" type="submit">Cancel reservation</button>
+            <button className="btn" type="button" onClick={() => cancelReservation(reservation.id)}>Cancel reservation</button>
           </div>
         </div>
       ))}
