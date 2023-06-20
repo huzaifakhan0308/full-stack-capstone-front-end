ef022957f8f9e576fc0f10568bcceaf6a56dfb82
Fene-87 <fenekayas87@gmail.com>
2023-06-20 18:16:27 +0300

    Implement styling for home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Carousel from 'react-multi-carousel';
+// import Carousel from 'react-multi-carousel';
 import { useDispatch, useSelector } from 'react-redux';
 import './Home.css';
 import 'react-multi-carousel/lib/styles.css';
@@ -9,25 +9,25 @@ import { fetchHotels } from '../../redux/home/homeSlice';
 import Loading from '../loading/Loading';
 
 const Home = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+  // const responsive = {
+  //   superLargeDesktop: {
+  //     // the naming can be any, depends on you.
+  //     breakpoint: { max: 4000, min: 3000 },
+  //     items: 5,
+  //   },
+  //   desktop: {
+  //     breakpoint: { max: 3000, min: 1024 },
+  //     items: 3,
+  //   },
+  //   tablet: {
+  //     breakpoint: { max: 1024, min: 464 },
+  //     items: 2,
+  //   },
+  //   mobile: {
+  //     breakpoint: { max: 464, min: 0 },
+  //     items: 1,
+  //   },
+  // };
 
   const loginUserData = JSON.parse(localStorage.getItem('user_data'));
 
@@ -45,16 +45,20 @@ const Home = () => {
   }
   return (
     <main className="home-container">
-      <h2 className="home-header">LATEST HOTELS</h2>
-      <p className="home-text">Please select a hotel</p>
+      <div className="bg-image">
+        <div className="header-background">
+          <h2 className="home-header">LATEST HOTELS</h2>
+          <p className="home-text">Please select a hotel</p>
+        </div>
+      </div>
 
-      <Carousel responsive={responsive} className="home-hotel-list">
+      <div className="home-hotel-list">
         {hotels.map((hotel) => (
           <NavLink key={hotel.id} to={`/details/${hotel.id}`} className="nav-link">
             <HotelCard {...hotel} />
           </NavLink>
         ))}
-      </Carousel>
+      </div>
     </main>
   );
 };
