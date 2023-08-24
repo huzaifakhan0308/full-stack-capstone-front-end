6bf2cc08821f64b5231eae34c46ba3a3ac4cc309
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-08-25 03:43:21 +0500

    finish details page styling

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {
   faBed, faCircleCheck, faCircleChevronRight, faGear, faTelevision, faWifi,
 } from '@fortawesome/free-solid-svg-icons';
@@ -5,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useParams } from 'react-router';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { fetchDetails, resetState } from '../../redux/details/detailsSlice';
 import Loading from '../loading/Loading';
 import './Details.css';
@@ -17,6 +18,7 @@ const Details = () => {
   const dispatch = useDispatch();
   const loginUserData = JSON.parse(localStorage.getItem('user_data'));
   const user = loginUserData?.user_id;
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(fetchDetails({ user_id: user, room_id: id }));
@@ -28,6 +30,11 @@ const Details = () => {
   if (status === 'loading') {
     return <Loading />;
   }
+
+  const navigation = (id) => {
+    navigate(`/AddReservation/${id}`);
+  };
+
   return (
     <div className="container-background" style={{ backgroundImage: `url(${room.image_url})` }}>
       <div className="details-container">
@@ -65,13 +72,11 @@ const Details = () => {
           {
             room.reservations_id === null ? (
               <div className="positioning">
-                <NavLink to={`/AddReservation/${id}`}>
-                  <div className="details-reserve-btn">
-                    <FontAwesomeIcon icon={faGear} />
-                    <p>reserve</p>
-                    <FontAwesomeIcon icon={faCircleChevronRight} />
-                  </div>
-                </NavLink>
+                <div className="details-reserve-btn" onClick={() => { navigation(id); }}>
+                  <FontAwesomeIcon icon={faGear} />
+                  <p>reserve</p>
+                  <FontAwesomeIcon icon={faCircleChevronRight} />
+                </div>
               </div>
             ) : (
               <div className="positioning">
