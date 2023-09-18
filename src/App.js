e9e77d31d4b182f76bad1fe11d5a1187fc679a4b
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-18 13:32:27 +0500

    remove basename from router

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const isLoggedIn = localStorage.getItem('logged_user') ? true : false;
   return (
     <div className="App">
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         {isLoggedIn && (
           <>
             <MobNav />
