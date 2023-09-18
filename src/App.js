505efe27bec4793b3e029fecedbae23de71d7d1d
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-18 13:11:41 +0500

    remove navigation from login page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const isLoggedIn = localStorage.getItem('logged_user') ? true : false;
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         {isLoggedIn && (
           <>
             <MobNav />
