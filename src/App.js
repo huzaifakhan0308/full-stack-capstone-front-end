25a0e17ac5b20599e369406cda091c1fd11b7c43
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-21 01:16:29 +0500

    trying to solve error

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
             path="/login"
             exact
             element={
-              <Login />
+              isLoggedIn ? <Navigate to="/" replace /> : <Login />
             }
           />
           <Route
