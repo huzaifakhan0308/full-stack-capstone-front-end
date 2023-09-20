95479c38fedf6dc3b45f430a5b941dd16e1e4b2b
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-21 01:43:45 +0500

    trying to solve error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-/* eslint-disabled */
+/* eslint-disable */
 import React, { useEffect } from 'react';
 import {
   BrowserRouter, Route, Routes, Navigate,
 } from 'react-router-dom';
-import Home from './pages/home/Home';
-import RoomList from './pages/RoomList';
-import Reservations from './pages/Reservations';
-import AddReservation from './pages/AddReservation';
+// import Home from './pages/home/Home';
+// import RoomList from './pages/RoomList';
+// import Reservations from './pages/Reservations';
+// import AddReservation from './pages/AddReservation';
+// import AddRoom from './pages/AddRoom';
+// import Details from './pages/details/Details';
 import NavbarPC from './components/NavbarPC';
 import './App.css';
 import MobNav from './components/MobNav';
-import AddRoom from './pages/AddRoom';
 import Login from './pages/Login';
-import Details from './pages/details/Details';
 
 function App() {
   const isLoggedIn = !!localStorage.getItem('logged_user');
@@ -27,19 +27,19 @@ function App() {
           </>
         )}
         <Routes>
-          <Route
+          {/* <Route
             path="/"
             element={
               <Home />
             }
-          />
+          /> */}
           <Route
             path="/login"
             element={
               <Login />
             }
           />
-          <Route
+          {/* <Route
             path="/rooms"
             element={
               <RoomList />
@@ -68,7 +68,7 @@ function App() {
             element={
               <Details />
             }
-          />
+          /> */}
         </Routes>
       </BrowserRouter>
     </div>
