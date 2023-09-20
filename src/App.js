a62a28fcdae7433eb4ae635a47169a3dd8a9d81e
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-21 02:09:57 +0500

    trying to solve error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 /* eslint-disable */
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
-  BrowserRouter, Route, Routes, Navigate,
+  BrowserRouter, Route, Routes
 } from 'react-router-dom';
-// import Home from './pages/home/Home';
-// import RoomList from './pages/RoomList';
-// import Reservations from './pages/Reservations';
-// import AddReservation from './pages/AddReservation';
-// import AddRoom from './pages/AddRoom';
-// import Details from './pages/details/Details';
-// import NavbarPC from './components/NavbarPC';
-// import MobNav from './components/MobNav';
 import './App.css';
 import Login from './pages/Login';
 import Abc from './pages/abc';
@@ -22,11 +14,11 @@ function App() {
         <Routes>
           <Route
             path="/login"
-            element={<Login />}
+            children={<Login />}
           />
           <Route
             path="/abc"
-            element={<Abc />}
+            children={<Abc />}
           />
         </Routes>
       </BrowserRouter>
